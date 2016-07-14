b8e3ef30873311d9db0d924014d5b4c24d471622
michal.rakoczy <rakomeister@gmail.com>
2016-07-14 18:07:53 +0200

    Dialog with Anurgath.

--- MRAN/MRAN/Dialog/MRAN.d
+++ MRAN/MRAN/Dialog/MRAN.d
@@ -1,111 +1,36 @@
-/*
-	Thea Brandon Initial Dialog
-*/
 BEGIN ~MRAN~
 
 IF ~NumTimesTalkedTo(0)~ THEN
 BEGIN Introduction
+	// What are you looking for?
 	SAY @200
-	
-	IF ~ReputationLT([PC], 6)~ THEN
-		REPLY ~Certainly, I'm <CHARNAME>. What are you doing here?~
-		GOTO IntroductionEvil
 		
-	IF ~ReputationGT([PC], 5)~ THEN
-		REPLY ~Certainly, I'm <CHARNAME>. What are you doing here?~
-		GOTO IntroductionGood
-	
 	IF ~~ THEN
-		REPLY ~Bugger off.~
-		EXIT
-		
-END
+		// Who are you?
+		REPLY @210
+		GOTO IntroductionRequested
 
-IF ~~ THEN
-BEGIN IntroductionGood
-	SAY ~<CHARNAME>? I must join your party!~
+	IF ~~ THEN
+		// I need to get to Kuldahar. Can you help me?
+		REPLY @220
+		SetGlobal("Exp_Pause","GLOBAL",1)
+		StartCutScene("BCtoKU")
+		EXIT
 	
 	IF ~~ THEN
-		REPLY ~I'd be happy to have you in my party.~
-		DO ~SetGlobal("KickedOut", "LOCALS", 0)~
-		DO ~JoinParty()~
-		EXIT
-		
-	IF ~~ THEN
-		REPLY ~No, thanks.~
+		// I'm just passing here. Goodbye.
+		REPLY @230
 		EXIT
 		
 END
 
 IF ~~ THEN
-BEGIN IntroductionEvil
-	SAY ~So *you* are <CHARNAME>? You will fall by my hand!~
-	
+BEGIN IntroductionRequested
+	// I'm Anurgath. I was sent by Argent to help the strangers.
+	SAY @300
+
 	IF ~~ THEN
-		DO ~Enemy()~
-		EXIT
-		
+		// Can I ask you a few questions?
+		REPLY @310
+		GOTO Introduction
 END
-
-IF ~NumTimesTalkedToGT(0) ReputationGT([PC], 5)~ THEN
-BEGIN IntroductionLater
-	SAY ~You're back. Are you ready to apologise?~
-	
-	IF ~~ THEN
-		REPLY ~Yes, I'm sorry. Do you want to join my party?~
-		DO ~SetGlobal("KickedOut", "LOCALS", 0)~
-		DO ~JoinParty()~
-		EXIT
-		
-	IF ~~ THEN
-		REPLY ~Bugger off.~
-		EXIT
-
-END
-
-/*
-	Thea Brandon Post Party Dialog
-*/
-BEGIN ~MRANP~
-
-IF ~Global("KickedOut", "LOCALS", 0)~ THEN
-BEGIN Kickout
-	SAY ~Oh, you don't need me no more?~
-	
-	IF ~~ THEN
-		REPLY ~No, I made a mistake. Please stay.~
-		DO ~JoinParty()~
-		EXIT
-		
-	IF ~~ THEN
-		REPLY ~Yes, it's time to part ways.~
-		GOTO KickoutFinal
-	
-END
-
-IF ~~ THEN
-BEGIN KickoutFinal
-	SAY ~Very well. I'll travel back to the Friendly Arms Inn.~
-	
-	IF ~~ THEN
-		DO ~SetGlobal("KickedOut", "LOCALS", 1)~
-		DO ~EscapeAreaMove("AR2100", 2702, 1673, 14)~
-		EXIT
-
-END
-
-IF ~Global("KickedOut", "LOCALS", 1)~ THEN
-BEGIN Rejoin
-	SAY ~You want me back in the party?~
-	
-	IF ~~ THEN
-		REPLY ~Yes, I need your services.~
-		DO ~SetGlobal("KickedOut", "LOCALS", 0)~
-		DO ~JoinParty()~
-		EXIT
-		
-	IF ~~ THEN
-		REPLY ~I don't think so.~
-		EXIT
-END
-
